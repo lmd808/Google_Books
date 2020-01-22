f6df3a70bdfe8de5ad8578023aca8527bce58d01
limabean <lditom808@gmail.com>
2020-01-21 20:32:32 -0500

    logic up to date. Prparing to imprelemt styles and passport logic

--- google_books/client/src/App.js
+++ google_books/client/src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-//import Saved from './components/save.js/';
-//import Search from './components/search.js/';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Pages/home';
+import Saved from './components/Pages/saved';
 import './App.css';
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div>
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/saved" component={Saved} />
+					</Switch>
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
